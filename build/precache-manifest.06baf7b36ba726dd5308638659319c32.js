c04634612b091ecfa243a825c4c55be6101cee76
Anonymous Authors <icml2020submission6857@gmail.com>
2020-02-06 15:38:03 -0800

    placeholder

---
+++ build/precache-manifest.06baf7b36ba726dd5308638659319c32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ee1fbbf75a6028c9b308dc99f94d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693ecb22cdbe2d398356",
+    "url": "/static/css/2.1d10ae30.chunk.css"
+  },
+  {
+    "revision": "2f95ea05cdd276919a80",
+    "url": "/static/css/main.6db8d7f3.chunk.css"
+  },
+  {
+    "revision": "693ecb22cdbe2d398356",
+    "url": "/static/js/2.5f90ad93.chunk.js"
+  },
+  {
+    "revision": "fc9dba852eaddf531193be703e8b0310",
+    "url": "/static/js/2.5f90ad93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f95ea05cdd276919a80",
+    "url": "/static/js/main.360b1c76.chunk.js"
+  },
+  {
+    "revision": "29a6f329d950adf383d9",
+    "url": "/static/js/runtime-main.db0fc1e0.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -15,23 +15,40 @@ function App() {
     let lastUpdateTime = mainRef['timestamp'];
     return (
         <div>
-            <Hero lastUpdateTime={lastUpdateTime} />
-            <div className="tabs is-centered">
-                <ul>
-                    <li className={(currentTab===1) ? "is-active": ""}>
-                        <a href="/" key={1} onClick={(e)=> {
-                        e.preventDefault();
-                        setCurrentTab(1);
-                    }}>1</a></li>
-                    <li className={(currentTab===2) ? "is-active": ""}>
-                        <a href="/" key={2} onClick={(e)=> {
-                            e.preventDefault();
-                            setCurrentTab(2);
-                        }}>2</a></li>
-                </ul>
+            {/*<Hero lastUpdateTime={lastUpdateTime} />*/}
+            {/*<div className="tabs is-centered">*/}
+            {/*    <ul>*/}
+            {/*        <li className={(currentTab===1) ? "is-active": ""}>*/}
+            {/*            <a href="/" key={1} onClick={(e)=> {*/}
+            {/*            e.preventDefault();*/}
+            {/*            setCurrentTab(1);*/}
+            {/*        }}>1</a></li>*/}
+            {/*        <li className={(currentTab===2) ? "is-active": ""}>*/}
+            {/*            <a href="/" key={2} onClick={(e)=> {*/}
+            {/*                e.preventDefault();*/}
+            {/*                setCurrentTab(2);*/}
+            {/*            }}>2</a></li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
+            {/*{(currentTab === 1) ? <Dashboard mainRef={mainRef} /> : null}*/}
+            {/*<Footer />*/}
+            <div className="box">
+                <h1 className="title has-text-centered">Demystifying Reproducibility in Meta- and Multi-Task Reinforcement Learning</h1>
+                <p>Establishing the significance of experimental results in reinforcement learning (RL) is difficult.
+                    This is compounded by the additional complexity of meta- and multi-task RL, a rapidly-growing
+                    research area which lacks well-defined baselines. We analyze several design decisions each author
+                    must make when they implement a meta-RL or MTRL algorithm, and show that these seemingly-small
+                    details can create variations in a single algorithm's performance that exceed the reported
+                    performance differences between algorithms themselves. Informed by this analysis, we precisely
+                    define several important hyperparameters, design decisions, and evaluation metrics for meta-RL
+                    and MTRL methods, so that we can compare these methods reproducibly. We then provide multi-seed
+                    benchmark results for seven of the most popular meta-RL and MTRL algorithms on the most challenging
+                    benchmarks currently available. Finally, we share with the community an open source package of these
+                    algorithm reference implementations, which use our consistent definitions, achieve
+                    state-of-the-art-performance, and seeks to follow the original works introducing these algorithms
+                    as closely as possible.
+                </p>
             </div>
-            {(currentTab === 1) ? <Dashboard mainRef={mainRef} /> : null}
-            <Footer />
         </div>
     );
 }
